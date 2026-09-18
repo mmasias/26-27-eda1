c38c5b1c6c1a7b365aae23f14b9e9f049631cc46
sergiofernandez1-hue <sergio.fernandez1@alumnos.uneatlantico.es>
2026-09-18 17:23:41 +0200

    extension reto

--- entregas/fernandezSergio/reto001.java
+++ entregas/fernandezSergio/reto001.java
@@ -2,6 +2,9 @@ import java.util.Scanner;
 
 class Cliente{
     private int productos;
+    private int minutosEsperando;
+    private double intentoAbandono;
+    private final double PROBABILIDAD_IRSE = 0.3;
 
     public Cliente(int productos){
         this.productos = productos;
@@ -11,16 +14,28 @@ class Cliente{
         return productos;
     }
 
+    public int getMinutosEsperando(){
+        return minutosEsperando;
+    }
+
     public void cobrarProductos(){
         final int PRODUCTOS_POR_SEGUNDO = 5;
         this.productos = productos - PRODUCTOS_POR_SEGUNDO;
+    }
+
+    public void espera(){
+        this.minutosEsperando = minutosEsperando + 1;
+    }
+
+    public boolean abandonarCola(){
+        this.intentoAbandono = Math.random();
+        return intentoAbandono <= PROBABILIDAD_IRSE;    
     }
 }
 
 public class reto001{
     static int ultimoPuesto = 0;
     static Cliente[] listaClientes = new Cliente [30];
-    static int clientesAtendidos = 0;
     
     public static void main(String[] args){    
 
@@ -31,18 +46,34 @@ public class reto001{
         Scanner scanner = new Scanner(System.in);    
 
         for(int minutos = 0 ; minutos <= 240 ; minutos++){
-
-            generarClientes(clienteNuevo, PROBABILIDAD_CLIENTE, cantidadProductos);  
-            avanzarCola();
+            
             dibujarCola(minutos);
+            generarClientes(clienteNuevo, PROBABILIDAD_CLIENTE, cantidadProductos);  
+            aumentarTiempoEspera();
+            avanzarCola();
 
+            if(minutos >= 20){
+                aburridometro();
+            }
             scanner.nextLine();
-        }
 
+        }
         scanner.close();
         System.out.println("El supermercado ha cerrado.");
     }
 
+    public static void dibujarCola(int minutos){
+        for(int i = 0; i <= listaClientes.length - 1; i++){
+            if(listaClientes[i] == null){
+            System.out.print("[ ]");
+
+            }else{
+                System.out.print("[" + listaClientes[i].getProductos() + ", " + listaClientes[i].getMinutosEsperando() + "]");
+            }
+        }
+        System.out.println("\nMinuto: " + minutos);
+    }    
+    
     public static void generarClientes(double clienteNuevo, double PROBABILIDAD_CLIENTE, int cantidadProductos){
         if (ultimoPuesto  <= listaClientes.length - 1){
             clienteNuevo = Math.random();
@@ -50,7 +81,6 @@ public class reto001{
             if (clienteNuevo <= PROBABILIDAD_CLIENTE){
                 cantidadProductos = (int) (Math.random() * 20) + 1;
                 listaClientes[ultimoPuesto] = new Cliente(cantidadProductos);
-
                 ultimoPuesto = ultimoPuesto + 1;
             }
         }
@@ -60,32 +90,42 @@ public class reto001{
         if(listaClientes[0] != null){
             if (listaClientes[0].getProductos() > 0){
                 listaClientes[0].cobrarProductos();
-
             }
             if (listaClientes[0].getProductos() <=0){
                 for(int i = 0; i <= listaClientes.length - 1; i++){
-                    if(i != 29){
+                    if(i != listaClientes.length - 1){
                         listaClientes[i] = listaClientes[i + 1];
-
                     }else{
                         listaClientes[i] = null;
                     }
                 }
 
                 ultimoPuesto = ultimoPuesto - 1;
-                clientesAtendidos = clientesAtendidos + 1;
             }                
         }
     }
 
-    public static void dibujarCola(int minutos){
-        for(int x = 0; x <= listaClientes.length - 1; x++){
-            if(listaClientes[x] == null){
-            System.out.print("[ ]");
-            }else{
-                System.out.print("[" + listaClientes[x].getProductos() + "]");
+    public static void aumentarTiempoEspera(){
+        for(int i = 0; i <= ultimoPuesto - 1; i++){
+            listaClientes[i].espera();
+        }
+    }
+
+    public static void aburridometro(){
+        for(int i = 1; i <= ultimoPuesto - 1; i++){
+            if(listaClientes[i].getMinutosEsperando() >= 8){
+                if(listaClientes[i].abandonarCola()){
+                    System.out.println("El cliente en el puesto " + i + " va a abandonar la cola.");
+                    for(int j = i; j <= ultimoPuesto - 1; j++){
+                        if (j != ultimoPuesto - 1){
+                            listaClientes[j] = listaClientes[j + 1];                    
+                        }else{
+                            listaClientes[j] = null;
+                        }
+                    }
+                    ultimoPuesto = ultimoPuesto - 1;
+                }
             }
         }
-        System.out.println("\nMinuto: " + minutos + "\nClientes atendidos: " + clientesAtendidos);
     }
 }
